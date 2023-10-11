7a85e71c4dc57bb7563f0485840f339bc2f0faaf
Furo <furogaming9@gmail.com>
2023-10-11 18:42:17 +0200

    Ajoutez des fichiers projet.

---
+++ Patches/DiscordPatch.cs
@@ -0,0 +1,72 @@
+using HarmonyLib;
+using Discord;
+using System;
+using AmongUs.Data;
+
+namespace TOHE.Patches
+{
+    // Originally from Town of Us Rewritten, by Det
+    [HarmonyPatch(typeof(ActivityManager), nameof(ActivityManager.UpdateActivity))]
+    public class DiscordRPC
+    {
+        private static string lobbycode = "";
+        private static string region = "";
+        public static Activity oldActivity = null;
+        public static void Prefix([HarmonyArgument(0)] Activity curActivity)
+        {
+            Activity activity = curActivity;
+            if (activity == null)
+            {
+                activity = oldActivity;
+            }
+            var details = $"TOHE v{Main.PluginDisplayVersion}";
+            activity.Details = details;
+            oldActivity = activity;
+
+            try
+            {
+                if (activity.State != "In Menus")
+                {
+                    if (!DataManager.Settings.Gameplay.StreamerMode)
+                    {
+                        int maxSize = GameOptionsManager.Instance.currentNormalGameOptions.MaxPlayers;
+                        if (GameStates.IsLobby)
+                        {
+                            lobbycode = GameStartManager.Instance.GameRoomNameCode.text;
+                            region = ServerManager.Instance.CurrentRegion.Name;
+                            if (region == "North America") region = "NA";
+                            if (region == "Europe") region = "EU";
+                            if (region == "Asia") region = "AS";
+                        }
+
+                        if (lobbycode != "" && region != "")
+                        {
+                            if (!Main.StreamerMode.Value)
+                            {
+                                details = $"TOHE - {lobbycode} ({region})";
+                            }
+                            else
+                            {
+                                details = $"TOHE - Code Hidden (Streamer Mode)";
+                            }
+                        }
+
+                        activity.Details = details;
+                    }
+                    else
+                    {
+                        details = $"TOHE - Code Hidden (Streamer Mode)";
+                    }
+                }
+            }
+
+            catch (ArgumentException ex)
+            {
+                Logger.Error("Error in updating discord rpc", "DiscordPatch");
+                Logger.Exception(ex, "DiscordPatch");
+                details = $"TOHE v{Main.PluginDisplayVersion}";
+                activity.Details = details;
+            }
+        }
+    }
+}
